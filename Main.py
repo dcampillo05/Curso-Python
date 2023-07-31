d582b7d341201e8f69aeac420bd73a048665d99f
Daniel Campillo Ricardi <dcampillo05@gmail.com>
2023-07-31 20:08:42 -0300

    first commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+velocidade = 61
+local_carro = 90
+
+RADAR = 60
+LOCAL1 = 100
+RADAR_RANGER = 1
+
+if velocidade > RADAR:
+    print(f'Velocidade do carro passou do limite {RADAR}')
+#if local_carro >= (LOCAL1 - RADAR_RANGER) and 
